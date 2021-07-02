181ecdc9d422a8a7e66d5446d81078ff4b3bad4a
rahulsinghbisht-13 <rbisht7332gmail.com>
2021-07-02 19:03:21 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 function App() {
   return (
     <div>
-      Phone-Directory
+      {/* <div className="header">
+        Phone-Directory
+      </div>
+
+      <button >Add</button>
+
+      <div>
+        <span>Name</span><br></br>
+        <span>Phone</span>
+      </div> */}
+      <label htmlFor="name">Name: </label>
+      <input id="name" type="text" placeholder="Type here" defaultValue="Rahul"/>
     </div>
   );
 }
-
 export default App;
