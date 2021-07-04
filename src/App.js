6bcc98f59a21b0fb4f6b201e8f5970c69d9651f0
rahulsinghbisht-13 <rbisht7332gmail.com>
2021-07-05 00:50:13 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import Header from "./Header";
 import './App.css';
 
 function App() {
+  let subscribers = [
+    {
+      id: 1,
+      name: "Rahul Singh Bisht",
+      phone: "8888888888"
+    },
+    {
+      id: 2,
+      name: "John Green",
+      phone: "9999999999"
+    }
+  ]
   return (
     <div id="parentDiv">
       <Header />
@@ -12,6 +24,15 @@ function App() {
           <span className="grid-item name-heading">Name</span>
           <span className="grid-item phone-heading">Phone</span>
         </div>
+
+        {
+          subscribers.map(sub => {
+            return <div key={sub.id} className="grid-container">
+            <span className="grid-item">{sub.name}</span>
+            <span className="grid-item">{sub.phone}</span>
+            </div>
+          })
+        }
       </div>
     </div>
   );
