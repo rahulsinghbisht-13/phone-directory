d628212b115e3112afec44d0dd7d7d43feb40cfb
rahulsinghbisht-13 <rbisht7332gmail.com>
2021-07-04 13:35:58 +0530

    Created 'Header' Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
+import Header from "./Header";
+
 function App() {
   return (
     <div>
-      {/* <div className="header">
-        Phone-Directory
-      </div>
-
+      <Header />
       <button >Add</button>
 
       <div>
         <span>Name</span><br></br>
         <span>Phone</span>
-      </div> */}
-      <label htmlFor="name">Name: </label>
-      <input id="name" type="text" placeholder="Type here" defaultValue="Rahul"/>
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// Functional component
+const Header = function() {
+    return (
+        <div className="header">
+            Phone-Directory
+        </div>
+    )
+}
+
+// Class Component
+
+// class Header extends Component {
+//     render() {
+//         return (
+//             <div className="header">
+//                 Phone-Directory
+//             </div>
+//         )
+//     }
+// }
+
+export default Header;
