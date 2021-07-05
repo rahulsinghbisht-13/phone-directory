261f877e7727e6456fc6037e586c911a8a1174cd
rahulsinghbisht-13 <rbisht7332gmail.com>
2021-07-05 13:19:00 +0530

    Added 'AddSubscriber' component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
 import Header from "./Header";
 import './App.css';
+import { Component } from "react";
 
-function App() {
-  let subscribers = [
-    {
-      id: 1,
-      name: "Rahul Singh Bisht",
-      phone: "8888888888"
-    },
-    {
-      id: 2,
-      name: "John Green",
-      phone: "9999999999"
-    }
-  ]
-  return (
-    <div id="parentDiv">
-      <Header heading="Phone Directory" />
-      <div className="component-body-container">
-        <button className="custom-btn add-btn">Add</button>
+class App extends Component{
 
-        <div className="grid-container heading-container">
-          <span className="grid-item name-heading">Name</span>
-          <span className="grid-item phone-heading">Phone</span>
+  render() {
+
+    let subscribers = [
+      {
+        id: 1,
+        name: "Rahul Singh Bisht",
+        phone: "8888888888"
+      },
+      {
+        id: 2,
+        name: "John Green",
+        phone: "9999999999"
+      }
+    ]
+
+    return (
+      <div id="parentDiv">
+        <Header heading="Phone Directory" />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+
+          {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+              </div>
+            })
+          }
         </div>
-
-        {
-          subscribers.map(sub => {
-            return <div key={sub.id} className="grid-container">
-            <span className="grid-item">{sub.name}</span>
-            <span className="grid-item">{sub.phone}</span>
-            <button id="delete">Delete</button>
-            </div>
-          })
-        }
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default App;
